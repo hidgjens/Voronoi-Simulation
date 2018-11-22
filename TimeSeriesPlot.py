8f146b11fa0c77542ed24be3ab1e2f0300ba1873
Jonathan <jonwb97@hotmail.co.uk>
2018-11-22 00:30:18 +0000

    Added possession region shading to plots

--- TimeSeriesPlot.py
+++ TimeSeriesPlot.py
@@ -33,17 +33,57 @@ def LoadByGame(filename):
     print('100%: finished.')
     return(df)
 
+def getPossessionRegions(df):
+    # take one player per frame
+    df = df.loc[(df['Team'] == 'Home') & (df['Num'] == 1)]
+    Frames = 1 + df['FID'].max()
+    startpoints = []
+    endpoints = []
+    colours = []
+
+    def possToColour(poss):
+        if poss:
+            return('r')
+        elif not poss:
+            return('b')
+        else:
+            print('Why am I here?')
+            return('g')
+        
+    # get possession in first frame, add startpoint and colour
+    poss = np.asscalar(df.loc[df['FID'] == 1, 'Possession'].values)
+    startpoints.append(1)
+    colours.append(possToColour(poss))
+
+    # find when possession changes, add region startpoint, endpoint and colour to list, then update possession
+    for i in range(1, Frames):
+        frameposs = np.asscalar(df.loc[df['FID'] == i, 'Possession'].values)
+        if frameposs != poss:
+            startpoints.append(i)
+            endpoints.append(i)
+            poss = frameposs
+            colours.append(possToColour(poss))
+
+    # add final endpoint 
+    endpoints.append(Frames)
+    print(list(zip(startpoints, endpoints, colours)))
+    return(list(zip(startpoints, endpoints, colours)))
+    
 # plots time series graphs for a given match
 def TimeSeriesPlot(df, var, title, filename):    
     # define useful quantities
     frames =  1 + df['FID'].max()
     players = df['Num'].max()
     t = range(1, frames) 
-    colours = cm.plasma(np.linspace(0, 1, players)) 
+    colours = cm.inferno(np.linspace(0, 1, players)) 
     patches = []
 
     print('Frames: %i' % frames)
     plt.figure(figsize = (21, 7), dpi = 300) 
+
+    # shade background regions
+    for s, e, c in getPossessionRegions(df):
+        plt.axvspan(s, e, facecolor = c, alpha = 0.4)
 
     for i in range(players):
         # get player data
@@ -62,7 +102,7 @@ def TimeSeriesPlot(df, var, title, filename):
     plt.xlabel('Frame')
     plt.ylabel(var)
     plt.title(title)
-    
+
     # legend
     plt.legend(handles = patches, loc = 1)
 
@@ -83,8 +123,12 @@ def TimeTeam(df, var, title, filename):
     print('Frames: %i' % frames)
     plt.figure(figsize = (21, 7), dpi = 300) 
     
+    # plot possession regions
+    for s, e, c in getPossessionRegions(df):
+        plt.axvspan(s, e, facecolor = c, alpha = 0.4)
+
     # plot
-    plt.plot(t, tmdata, linestyle = '-', linewidth = 0.75)
+    plt.plot(t, tmdata, linestyle = '-', linewidth = 0.75, color = 'k')
 
     # formatting
     plt.xlim(0,)
